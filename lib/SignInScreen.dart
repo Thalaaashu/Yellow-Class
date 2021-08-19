955499c33359eddacfd511953e8fdc983791ea52
Ashish Singh <63043086+Thalaaashu@users.noreply.github.com>
2021-08-19 17:14:38 +0530

    first commit

--- lib/SignInScreen.dart
+++ lib/SignInScreen.dart
@@ -21,90 +21,99 @@ class _SignInScreenState extends State<SignInScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
+      body: Container(
         width: double.infinity,
         height: double.infinity,
         decoration: BoxDecoration(
-        gradient: LinearGradient(
-        colors: [
-        Colors.blue,
-        Colors.red,
-        ],
-    ),
-    ),
-    child: Card(
-    margin: EdgeInsets.only(top: 200, bottom: 200, left: 30, right: 30),
-    elevation: 20,
-    child: Column(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: [
-    Text(
-    "My Movies",
-    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-    ),
-    Padding(
-    padding: const EdgeInsets.only(left: 20, right: 20),
-    child: MaterialButton(
-    color: Colors.teal[100],
-    elevation: 10,
-    child: Row(
-    mainAxisAlignment: MainAxisAlignment.start,
-    children: [
-    Container(
-    height: 30.0,
-    width: 30.0,
-    decoration: BoxDecoration(
-    //image: DecorationImage(
-    //image:
-    //AssetImage('assets/images/googleimage.png'),
-    //fit: BoxFit.cover),
-    shape: BoxShape.circle,
-    ),
-    ),
-    SizedBox(
-    width: 20,
-    ),
-    Text("Sign In with Google")
-    ],
-    ),
+          gradient: LinearGradient(
+            colors: [
+              Colors.blue,
+              Colors.red,
+            ],
+          ),
+        ),
+        child: Card(
+          margin: EdgeInsets.only(top: 200, bottom: 200, left: 30, right: 30),
+          elevation: 20,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                "My Movies",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: MaterialButton(
+                  color: Colors.teal[100],
+                  elevation: 10,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 30.0,
+                        width: 30.0,
+                        decoration: BoxDecoration(
+                          //image: DecorationImage(
+                          //image:
+                          //AssetImage('assets/images/googleimage.png'),
+                          //fit: BoxFit.cover),
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text("Sign In with Google")
+                    ],
+                  ),
 
-    // by onpressed we call the function signup function
-    onPressed: (){
-    signup(context);
-  },
-  ),
-  )
-  ],
-  ),
-  ),
-  ),
-  );
-}
+                  // by onpressed we call the function signup function
+                  onPressed: () {
+                    signup(context);
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
 // function to implement the google signin
 
 // creating firebase instance
-final FirebaseAuth auth = FirebaseAuth.instance;
+  final FirebaseAuth auth = FirebaseAuth.instance;
 
-Future<void> signup(BuildContext context) async {
-  final GoogleSignIn googleSignIn = GoogleSignIn();
-  final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
-  if (googleSignInAccount != null) {
-    final GoogleSignInAuthentication googleSignInAuthentication =
-    await googleSignInAccount.authentication;
-    final AuthCredential authCredential = GoogleAuthProvider.credential(
-        idToken: googleSignInAuthentication.idToken,
-        accessToken: googleSignInAuthentication.accessToken);
+  Future<void> signup(BuildContext context) async {
+    try {
+      final GoogleSignIn googleSignIn = GoogleSignIn();
+      final GoogleSignInAccount googleSignInAccount = await googleSignIn
+          .signIn();
+      //if (googleSignInAccount != null) {
+      final GoogleSignInAuthentication googleSignInAuthentication =
+      await googleSignInAccount.authentication;
+      final AuthCredential authCredential = GoogleAuthProvider.credential(
+          idToken: googleSignInAuthentication.idToken,
+          accessToken: googleSignInAuthentication.accessToken);
 
-    // Getting users credential
-    UserCredential result = await auth.signInWithCredential(authCredential);
-    User user = result.user;
+      // Getting users credential
+      UserCredential result = await auth.signInWithCredential(authCredential);
+      User user = result.user;
 
-    if (result != null) {
+      //if (result != null) {
       Navigator.pushReplacement(
           context, MaterialPageRoute(builder: (context) => HomePage()));
-    } // if result not null we simply call the MaterialpageRoute,
-    // for go to the HomePage screen
-  }
-}
+      //} // if result not null we simply call the MaterialpageRoute,
+      //}
+      //catch(Exception e){}// for go to the HomePage screen
+    } on Exception catch (_) {}
 
-}
+    finally {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => HomePage()));}
+    }
+  }
+
+
